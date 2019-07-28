c6abe02e11fb7cdc7fcb5a3babbad80b20df529f
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-28 20:35:13 +0000

    Automatically backed up by Learn

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,5 +1,13 @@
 class Member
   attr_accessor :name, :power, :bio
   
+  @@all = []
+  
+  def initialize(attr)
+    @name = attr[:name]
+    @power = attr[:power]
+    @bio = attr[:bio]
+    @@all << self
+  end
   
 end
