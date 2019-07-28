bfd095e1beb1a554130100a7427bc07672bcc280
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-28 20:32:08 +0000

    Automatically backed up by Learn

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,1 +1,15 @@
 class Team
+  attr_accessor :name, :motto, :members
+  
+  @@all = []
+  
+  def initialize(attr)
+    @name = attr[:name]
+    @motto = attr[:motto]
+    @members = []
+    @@all << self
+  end
+  
+  def self.
+  
+end
